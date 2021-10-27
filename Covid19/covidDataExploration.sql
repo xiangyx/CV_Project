ea4fbf745a84943a62736c9611f9304d8e8c2b55
xiangyx <xiangyx7@gmail.com>
2021-10-27 21:25:00 +0200

    Tableau and RL

--- Covid19/covidDataExploration.sql
+++ Covid19/covidDataExploration.sql
@@ -1,4 +1,4 @@
-use PortfolioProject;
+use PortfolioProject
 
 select *
 from covid_deaths
@@ -215,3 +215,31 @@ where vac.continent is not null
 --	where continent is null and population is not null
 --	group by location) as cd
 --order by death_rate desc;
+
+Select SUM(new_cases) as total_cases, 
+	   SUM(cast(new_deaths as int)) as total_deaths,
+	   SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From covid_deaths
+where continent is not null 
+order by 1,2
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From covid_deaths
+Where continent is null 
+and location not in ('World', 'European Union', 'International')
+Group by location
+order by TotalDeathCount desc
+
+Select Location, Population,
+	   MAX(total_cases) as HighestInfectionCount,
+	   Max((total_cases/population))*100 as PercentPopulationInfected
+From covid_deaths
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+Select Location, Population,date, 
+	   MAX(total_cases) as HighestInfectionCount,  
+	   Max((total_cases/population))*100 as PercentPopulationInfected
+From covid_deaths
+Group by Location, Population, date
+order by PercentPopulationInfected desc
